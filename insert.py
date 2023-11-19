9bedf296406c6dba04c682d291e763c86ea10404
Br4yanGC <brayan20160113@gmail.com>
2023-11-18 20:50:24 -0500

    update

--- insert.py
+++ insert.py
@@ -7,15 +7,11 @@ with open('data.json', 'r') as file:
 
 # Connect to the MongoDB server
 client = MongoClient('192.168.0.102', 30000)
-
 # Create or get the database
 db = client['lab16']
-
 # Create or get the collection
 collection = db['weather']
-
 # Insert the data into the collection
 collection.insert_many(data)
-
 # Close the MongoDB connection
 client.close()
